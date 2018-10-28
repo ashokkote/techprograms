edef49d55269229241f20c4d690403121193d349
Ashoka Kote <ashoka.kote@gmail.com>
2018-10-27 23:57:22 -0400

    Add back deleted classes from right Username

---
+++ src/com/kote/tech/math/EvenFibnocciSum.java
@@ -0,0 +1,42 @@
+package com.kote.tech.math;
+ 
+public class EvenFibnocciSum {
+	
+	public int calculateSumOfEvenFibnocci(int range) {
+		
+		int sum = 0;
+		int evensum = 0;	
+		int previousNumber = 0;
+		int nextNumber = 1;
+		
+		String numbers = "";	
+		
+		for(int i=1; i<=range; i++) {
+			
+			sum = previousNumber + nextNumber;
+            previousNumber = nextNumber;
+            nextNumber = sum;
+            
+            if(sum%2==0){
+            	evensum += sum;
+            }
+                        
+			numbers = numbers + sum + "," ;
+		}
+		
+		System.out.println("numbers = "+numbers);
+		
+		return evensum;
+	}
+	
+	
+ 	public static void main(String[] args) {
+		
+		EvenFibnocciSum sumOfFibnocci = new EvenFibnocciSum();
+		
+		int sum = sumOfFibnocci.calculateSumOfEvenFibnocci(20);
+		
+		System.out.println("Even Sum = "+sum);
+		
+ 	}
+ }
